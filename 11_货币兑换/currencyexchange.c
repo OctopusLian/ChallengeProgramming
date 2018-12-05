5d929479fbfb62f0b9c66b2f80c54baeb117ff0e
OctopusLian <zoctopus@qq.com>
2018-12-05 18:57:37 +0800

    添加10、11练习

---
+++ 11_货币兑换/currencyexchange.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char *argv[])
+{
+
+    float amount_to,amount_from,rate_from,rate,to; //美元数，欧元数，欧元的当前汇率，美元的当前汇率
+
+    printf("How many euros are you exchanging?"); scanf("%f",&amount_from);
+    printf("What is the exchange rate?"); scanf("%f",&rate_from);
+
+}
+
+
+/*
+美元欧元转换过程应先了解下
+*/
